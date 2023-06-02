21554c18d36a19b98324d787170d021f49faac3a
19Fabio84-hue <piancatellif@gmail.com>
2023-06-02 21:17:41 +0200

    .

--- src/component/domanda.js
+++ src/component/domanda.js
@@ -4,9 +4,9 @@ import React from 'react'
 export default function Domanda(props){
    
     return (
-       <>
+       <div className='main-ctn'>
        <h2  className='main' key={props.id} onClick={props.toggle}>{props.value}</h2>
        
-       </>
+       </div>
         )
 }
